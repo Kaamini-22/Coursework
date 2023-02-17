6c12a2e52b015dbdd3baa98cff17df94478b173a
Kaamini <40578064@live.napier.ac.uk>
2023-02-17 11:40:55 +0000

    Initial Commit...

---
+++ src/main/java/Code1/Intro.java
@@ -0,0 +1,16 @@
+package Code1;
+
+/**
+ * Hello Group 5
+ */
+
+public class Intro {
+
+    public static void main(String[] args){
+        System.out.println("Boo yah!");
+        System.out.println("Hello Group 5 SEM");
+        System.out.println("Group members: ");
+        System.out.println(" ");
+        System.out.println("Kaamini " + "Emillia " + "Valentin " + "Matthew " + "Rajan ");
+    }
+}
