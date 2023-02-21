4ce852c9a0e6f9e3f304c0a7e3c35368c3c20560
Rajan Poudel <40542238@live.napier.ac.uk>
2023-02-21 10:12:11 +0000

    team members

---
+++ src/main/java/Code1/teammembers.java
@@ -0,0 +1,10 @@
+package Code1;
+
+public class teammembers {
+
+        public static void main(String[] args)
+        {
+            System.out.println("Kaamini " + "Emillia" +"Valentin" +"Matthew" +"Rajan");
+        }
+    }
+
